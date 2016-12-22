987b4dc6638efcc05f635bd350822ba70abd5488
Asetr <asetre.paul@gmail.com>
2016-12-22 11:44:24 -0800

    cors fix

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 var statePrices = require('./app.js');
 var stateGasPrices;
@@ -10,11 +11,7 @@ function callback(info) {
 	stateGasPrices = info;
 }
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 
 app.get('/:state', function(req, res){
 	var state = req.params.state;
