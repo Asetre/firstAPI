85840afe45d48d658cb33ed7b656894b1573477c
Asetr <asetre.paul@gmail.com>
2016-12-22 11:36:46 -0800

    Cors fix

--- main.js
+++ main.js
@@ -10,6 +10,12 @@ function callback(info) {
 	stateGasPrices = info;
 }
 
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/:state', function(req, res){
 	var state = req.params.state;
 	res.json(stateGasPrices[state]);
